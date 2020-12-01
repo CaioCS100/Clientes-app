d33f14036c0ffc4ff014dfea1ca55a125fa10560
Caio Cesar Silva <caio.cesar.s100@gmail.com>
2020-12-01 12:00:32 -0300

    Adicionando componente home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { TemplateModule } from './template/template.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
